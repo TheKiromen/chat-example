c23ccdce483e446c99fc006f5614d765d0e59ad8
Dominik Kruczek <TheKiromen@gmail.com>
2022-05-09 17:03:25 +0200

    Major refactoring
    Implemented user list
    Simplified channel switching
    Added comments
    Added instructions what to do and fix

--- index.js
+++ index.js
@@ -3,29 +3,54 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
+let users = new Map();
+
+//TODO Wysyłać nie sam text ale obiekty z timestampem?
+//TODO Spróbować podpiąć SocketIO admin-ui
+
+//FIXME Pod koniec pousuwać niepotrzebne console.logi
+
+//Klient łączy się z serwerem
+io.on('connection', (socket) => {
+
+  //Dołącz do serwera
+  socket.on('join',(username,channel)=>{
+    //Dołącz do kanału
+    socket.join(channel);
+    //Dodaj do listy klientów
+    users.set(socket.id,username);
+    //Wyślij klientom nową listę userów
+    //TODO
+  })
+
+
+  //Wyjdź z serwera
+  socket.on('disconnect',()=>{
+    //Usuń z listy klientów
+    users.delete(socket.id);
+    //Wyślij klientom nową listę userów
+    //TODO
+  })
+
+  //Rozsyłanie wiadomości
+  socket.on('chat message', (msg, active_channel,timestamp) => {
+    //Roześlij wiadomość do wszystkich w kanale
+    //FIXME ustawić żeby wysyłało też do samego siebie
+    socket.to(active_channel).emit('chat message', users.get(socket.id),msg,timestamp);
+  });
+
+  //Zmień kanał
+  socket.on('changeRoom',(room)=>{
+    //Opuść aktualny kanał
+    socket.leave(Array.from(socket.rooms)[1]);
+    //Dołącz do nowego kanału
+    socket.join(room);
+  })
+});
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
-
-io.on('connection', (socket) => {
-  //otrzymaj wiadomość
-  socket.on('chat message', (msg, active_channel) => {
-    console.log(msg, active_channel)
-    //jeśli brak kanału do wszystkich
-    if(active_channel === ""){
-      io.emit('chat message', msg);
-    }
-    //jeśli kanał do kanału
-    else {
-      socket.to(active_channel).emit('chat message', msg)
-    }
-  });
-  //dołącz do kanału
-  socket.on('join', (room) => {socket.join(room);});
-  //odłącz od kanału
-  socket.on('leave', (room) => {socket.leave(room);});
-});
-
 
 http.listen(port, () => {
   console.log(`Socket.IO server running at http://localhost:${port}/`);
